088a91b38a1ea709d33014e85b2ff177d5f3ac1c
RobertWalaitis <rwalaitis@gmail.com>
2025-11-22 03:50:29 -0600

    no

--- server/server.js
+++ server/server.js
@@ -16,7 +16,6 @@ app.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", "https://robertwalaitis.github.io");
   res.header("Access-Control-Allow-Headers", "Content-Type");
   res.header("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE");
-  console.log("Incoming request:", req.url);
   next();
 });
 app.use(express.json());
