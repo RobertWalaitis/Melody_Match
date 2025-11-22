83149900a0de17adfb02298ff699d03d6f4bf63b
RobertWalaitis <rwalaitis@gmail.com>
2025-11-22 01:36:48 -0600

    ugh

--- server/server.js
+++ server/server.js
@@ -12,6 +12,12 @@ app.use(cors({
   methods: "GET,POST,PUT,DELETE",
   allowedHeaders: "Content-Type"
 }));
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "https://robertwalaitis.github.io");
+  res.header("Access-Control-Allow-Headers", "Content-Type");
+  res.header("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE");
+  next();
+});
 app.use(express.json());
 
 let db;
