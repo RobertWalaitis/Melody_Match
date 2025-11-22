2299b1252b3457c0d39941d4fb665d004f2c9779
RobertWalaitis <rwalaitis@gmail.com>
2025-11-22 00:46:49 -0600

    make sure not to make the same table

--- server/setup.sql
+++ server/setup.sql
@@ -1,17 +1,17 @@
-CREATE TABLE Profiles (
+CREATE TABLE IF NOT EXISTS Profiles (
     profile_id INTEGER PRIMARY KEY,
     profile_name varchar(20),
     profile_password varchar(20)
 );
 
-CREATE TABLE Song (
+CREATE TABLE IF NOT EXISTS Song (
     song_id INTEGER PRIMARY KEY,
     title varchar(100),
     song_length INTEGER,
     artist varchar(100)
 );
 
-CREATE TABLE Liked (
-    liked_song_id INTEGER references Song(song_id),
-    profile_user_id INTEGER references Profiles(profile_id)
+CREATE TABLE IF NOT EXISTS Liked (
+    liked_song_id INTEGER REFERENCES Song(song_id),
+    profile_user_id INTEGER REFERENCES Profiles(profile_id)
 );
