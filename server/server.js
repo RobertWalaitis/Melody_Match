005e40e8336fb48c1498b108cef9433a6a7f9f28
RobertWalaitis <rwalaitis@gmail.com>
2025-12-05 20:31:17 -0600

    but again

--- server/server.js
+++ server/server.js
@@ -7,24 +7,23 @@ import songRouter from "./routes/songs.js";
 import likedRouter from "./routes/liked.js";
 
 async function startServer() {
-  const app = express();
-
-  app.use(cors({
-    origin: "https://robertwalaitis.github.io",
-    methods: "GET,POST,PUT,DELETE",
-    allowedHeaders: "Content-Type"
-  }));
-
-  app.use((req, res, next) => {
-    res.header("Access-Control-Allow-Origin", "https://robertwalaitis.github.io");
-    res.header("Access-Control-Allow-Headers", "Content-Type");
-    res.header("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE");
-    next();
-  });
-
-  app.use(express.json());
-
   try {
+    const app = express();
+
+    app.use(cors({
+      origin: "https://robertwalaitis.github.io",
+      methods: "GET,POST,PUT,DELETE",
+      allowedHeaders: "Content-Type"
+    }));
+
+    app.use((req, res, next) => {
+      res.header("Access-Control-Allow-Origin", "https://robertwalaitis.github.io");
+      res.header("Access-Control-Allow-Headers", "Content-Type");
+      res.header("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE");
+      next();
+    });
+
+    app.use(express.json());
     const db = await initDB();
     await db.exec("PRAGMA foreign_keys = ON;");
 
